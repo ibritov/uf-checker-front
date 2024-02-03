5fa2f50a1b8b43f07de5ede6429700f369ce63d7
Isaias <Brito>
2024-02-02 23:47:14 -0300

    remove code

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -2,7 +2,6 @@
     <div class="min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-800">
       <div class="bg-white dark:bg-gray-700 p-8 rounded shadow-md max-w-md w-full">
         <h1 class="text-2xl font-semibold mb-4">Vista de Administrador</h1>
-        <button @click="toggleShowMenu">Toggle Show Menu</button>
       </div>
     </div>
   </template>
@@ -11,17 +10,7 @@
   import { mapMutations } from 'vuex';
   
   export default {
-    methods: {
-      ...mapMutations(['setShowMenu']),
-      toggleShowMenu() {
-        this.setShowMenu(!this.showMenu);
-      },
-    },
-    computed: {
-      showMenu() {
-        return this.$store.state.showMenu;
-      },
-    },
+    
   };
   </script>
   
